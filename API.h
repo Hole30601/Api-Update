5c92a273271543186d822d569f8ae09a97d6cc33
Hole 30601 <121752219+Hole30601@users.noreply.github.com>
2024-09-03 16:05:24 +0700

    Add files via upload

---
+++ API.h
@@ -0,0 +1,14 @@
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+@interface API : NSObject
+NS_ASSUME_NONNULL_BEGIN
+
+- (void)paid:(void (^)(void))execute;
+- (void) setToken:(NSString*)settoken;
+- (NSString*)getUDID;
+- (NSString *)getDeviceName;
+- (NSString *)getModel;
+- (NSString *)getVersionIOS;
+- (NSString *)getKey;
+@end
+NS_ASSUME_NONNULL_END
